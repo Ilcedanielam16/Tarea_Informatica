4a8ad3c38da1f56f33904130886c4e07d2bebdef
Ilcedanielam16 <78665970+Ilcedanielam16@users.noreply.github.com>
2021-04-11 00:16:09 -0600

    Actualización del main.cpp
    Se realizó un programa que invierte los numeros ingresados usando funciones.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
-// <Ingrese su nombre completo y número de carnet>
+// <Ilce Daniela Morales Areaga, 1650521>
 #include <iostream>
-
 using namespace std;
-
-int main()
+int Inverso(int numero)
 {
-    cout << "¡Hola estudiantes!" << endl;
-    return 0;
+	int res;
+	int invertir = 0;
+	while (numero > 0 )
+	{
+		res = numero % 10;
+		numero = numero / 10;
+		invertir = invertir * 10 + res;
+
+	}
+	
+	return invertir;
+}
+int main() {
+
+	int numero;
+	cout << "ingrese una cantidad que desea invertir" << endl;
+	cin >> numero;
+
+	cout << "El numero invertido es: " << Inverso(numero) << endl;
+
+	
 }
